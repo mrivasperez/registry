a49fb6b6c88b2bcb4bf571005e9fe489f91b8e71
migue <35717991+mrivasperez@users.noreply.github.com>
2024-12-07 14:08:04 -0800

    feat: Search Results (#2)
    * chore: confirm queryParams are consumable
    
    * chore: add search types
    
    * api: fetch results
    
    * feat: load search results

--- src/app/pages/search-results/search-results.component.ts
+++ src/app/pages/search-results/search-results.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PackageSummary, SearchResponse } from './types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-results',
   imports: [],
   templateUrl: './search-results.component.html',
-  styleUrl: './search-results.component.css'
+  styleUrl: './search-results.component.css',
 })
-export class SearchResultsComponent {
+export class SearchResultsComponent implements OnInit {
+  results: PackageSummary[] | undefined = undefined;
+  searchTerm = '';
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const searchTerm = params['query'];
+      this.searchTerm = searchTerm;
+      console.log(searchTerm);
+
+      if (searchTerm) {
+        fetch(`https://registry.npmjs.org/-/v1/search?text=${searchTerm}`)
+          .then((response) => response.json())
+          .then((data: SearchResponse) => {
+            // console.log(data.objects);
+            const results = data.objects.map(
+              ({ package: { name, description, version, keywords } }) => {
+                return {
+                  name,
+                  description,
+                  version,
+                  keywords,
+                };
+              }
+            );
+
+            this.results = results;
+          });
+      }
+    });
+  }
 }
