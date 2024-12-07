1e1e283d6181b43cc60fe9e6780e2772c011d71b
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-07 13:34:27 -0800

    chore: confirm queryParams are consumable

--- src/app/pages/search-results/search-results.component.ts
+++ src/app/pages/search-results/search-results.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
   imports: [],
   templateUrl: './search-results.component.html',
-  styleUrl: './search-results.component.css'
+  styleUrl: './search-results.component.css',
 })
-export class SearchResultsComponent {
+export class SearchResultsComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const searchTerm = params['query'];
+      console.log(searchTerm)
+    });
+  }
 }
