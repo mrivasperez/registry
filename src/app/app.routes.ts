813fe42c5b9d8725c2e2834c6b75356a62381edf
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-07 12:24:14 -0800

    task: route to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchResultsComponent } from './pages/search-results/search-results.component';
+import { PackageInfoComponent } from './pages/package-info/package-info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchResultsComponent },
+  { path: 'package/:packageName', component: PackageInfoComponent },
+];
