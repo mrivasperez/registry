a69e9ef6d5a0c0efa33f9c0645467e18aea23c2a
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-07 12:33:35 -0800

    chore: validate params are consumable

--- src/app/pages/package-info/package-info.component.ts
+++ src/app/pages/package-info/package-info.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-package-info',
   imports: [],
   templateUrl: './package-info.component.html',
-  styleUrl: './package-info.component.css'
+  styleUrl: './package-info.component.css',
 })
 export class PackageInfoComponent {
+  title = '';
 
+  constructor(@Inject(ActivatedRoute) private route: ActivatedRoute) {
+    this.route.paramMap.subscribe((params) => {
+      const packageName = params.get('packageName');
+      this.title = `Package: ${packageName}`;
+    });
+  }
 }
