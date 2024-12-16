e5d62f1434c93cba573f69e201989f945ff6ca13
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-16 11:12:58 -0800

    task: build component logic

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  searchTerm = new FormControl('');
+  constructor(private router: Router) {}
+  onSearch(event: Event): void {
+    const query = this.searchTerm.value;
+    event.preventDefault(); // Prevent default form submission
+    if (query) {
+      this.router.navigate(['/search'], {
+        queryParams: { query },
+      });
+    }
+  }
 }
